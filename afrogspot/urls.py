574fe9dbd0c6824ade6c4e01d6b227616a0f2485
Lilian Amatobi <lilianadimchinobi@gmail.com>
2024-09-05 10:37:25 +0000

    Added product detail functionality

--- afrogspot/urls.py
+++ afrogspot/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('product_catalog/', include('product_catalog.urls')),
+    path('product_catalog/<int:product_id>/', include('product_catalog.urls')),
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- product_catalog/views.py
+++ product_catalog/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Product #Rating
 
 # Create your views here.
 def product_list(request):
-    """ A view to show list of products"""
+    """ A view to show list of products that are available"""
+
     products = Product.objects.filter(is_active=True).order_by('name')
 
     context = {
@@ -11,3 +12,17 @@ def product_list(request):
     }
 
     return render(request, 'product_catalog/product_list.html', {'products': products})
+
+
+def product_detail(request, product_id):
+    """ A view to show individual products"""
+
+    product = get_object_or_404(Product, id=product_id)
+    #reviews = Rating.objects.filter(product=product).order_by('-created_at')
+
+    context = {
+        'product': product,
+        #'reviews': reviews,
+    }
+
+    return render(request, 'product_catalog/product_detail.html', {'product': product}) # 'reviews': reviews})
