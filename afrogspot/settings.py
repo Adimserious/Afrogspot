093fd714378aaa8f12c0799edc5f48b257d5e100
Lilian Amatobi <lilianadimchinobi@gmail.com>
2024-09-23 16:02:40 +0000

    Removed secret key and set debug

--- afrogspot/settings.py
+++ afrogspot/settings.py
@@ -36,10 +36,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_yayi+d+lpuy5bv^-_)#_74=p6+7q5=1xun@zppn$$$w4lc9nh'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['8000-adimserious-afrogspot-w484q2uf062.ws.codeinstitute-ide.net', 'afrogspot-e3f40930991f.herokuapp.com']
 
