9806b5c6fe0a5b85d3a696b8d350090353d2c872
Lilian Amatobi <lilianadimchinobi@gmail.com>
2024-09-23 08:54:17 +0000

    Add default image

--- product_catalog/models.py
+++ product_catalog/models.py
@@ -35,7 +35,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     discount_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     stock = models.IntegerField()
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True, default='static/media/background-image.jpg')
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
