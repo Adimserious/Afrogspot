8ac4e625194b9cec5102808e57cac525504dec08
Lilian Amatobi <lilianadimchinobi@gmail.com>
2024-09-24 11:09:35 +0000

    Collected static files for deployment

--- afrogspot/settings.py
+++ afrogspot/settings.py
@@ -201,6 +201,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 #STATICFILES_DIRS = [BASE_DIR / "static"]
+if not 'USE_AWS' in os.environ:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -235,8 +238,8 @@ if 'USE_AWS' in os.environ:
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-
-
+    AWS_DEFAULT_ACL = 'public-read'
+    
 # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
