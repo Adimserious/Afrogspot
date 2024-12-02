4427217bd0b25b9cf38bfa29dcbca10965262da8
Lilian Amatobi <lilianadimchinobi@gmail.com>
2024-12-02 21:41:25 +0000

    Adjust second nav bar

--- afrogspot/settings.py
+++ afrogspot/settings.py
@@ -68,8 +68,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY',
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-DEBUG = 'DEVELOPMENT' in os.environ
 
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-adimserious-afrogspot-2xe5zj7t4ro.ws.codeinstitute-ide.net', 'afrogspot-e3f40930991f.herokuapp.com']
 
