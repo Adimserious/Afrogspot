bf8f89090925a82a5c3e8f89dcae08e556cea9f4
Lilian Amatobi <156024751+Adimserious@users.noreply.github.com>
2025-10-31 20:47:34 +0100

    Update apps.py
    TEMP: disable signals on Heroku so we can run migrations on empty DB

--- product_catalog/apps.py
+++ product_catalog/apps.py
@@ -1,10 +1,9 @@
 from django.apps import AppConfig
 
-
 class ProductCatalogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'product_catalog'
 
-
-    def ready(self):
-        import product_catalog.signals
+    # TEMP: disable signals on Heroku so we can run migrations on empty DB
+    # def ready(self):
+    #     import product_catalog.signals
