a4938b0fde701073569bd7d9a7a641324de3d4c7
Lilian Amatobi <156024751+Adimserious@users.noreply.github.com>
2025-11-01 13:42:30 +0100

    Update apps.py
    Reanable signal

--- product_catalog/apps.py
+++ product_catalog/apps.py
@@ -4,6 +4,6 @@ class ProductCatalogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'product_catalog'
 
-    # TEMP: disable signals on Heroku so we can run migrations on empty DB
-    # def ready(self):
-    #     import product_catalog.signals
+    # Re-enable signals now that the DB is ready
+    def ready(self):
+        import product_catalog.signals
